e72860da0615aeb76fc49df054501033974af134
PrimusLV <k.drivnieks@inbox.lv>
2015-12-24 21:50:25 +0200

    Update DefaultProvider.php

--- src/primus/ultrafactions/provider/DefaultProvider.php
+++ src/primus/ultrafactions/provider/DefaultProvider.php
@@ -15,8 +15,8 @@ class DefaultProvider implements Provider {
     
     $this->plugin->saveResource("groups.yml");
     
-    $this->groups = new Config($this->plugin->getDataFolder() . "groups.yml", Config::YAML, []);
-    $this->userDataFolder = $this->plugin->getDataFolder() . "players/";
+    $this->factions = new Config($this->plugin->getDataFolder() . "groups.yml", Config::YAML, []);
+    $this->memberDataFolder = $this->plugin->getDataFolder() . "members/";
     
     if(!file_exists($this->userDataFolder)) \mkdir($this->userDataFolder, 0777, true);
   }
@@ -25,8 +25,33 @@ class DefaultProvider implements Provider {
     return $this->getResource()->get($member->getPlayer()->getName());
   }
   
-  public function setMemberdata(Member)
+  public function setMemberData(Member $member, array $data){
+    $this->getResource()->set($member->getPlayer()->getName(), $data);
+  }
   
+  public function removeMemberData(Member $member){
+    $this->getResource()->set($member->getPlayer()->getName(), []);
+  }
+  
+  // Factions
+  
+  public function getFactionData(Faction $faction){
+    return $this->getResource()->get($faction->getName());
+  }
+  
+  public function setFactionData(Faction  $faction){
+    $this->getResource()->set($faction->getName(), $data);
+  }
 
-
+  public function removeFactionData(Faction $faction){
+    $this->getResource()->set($faction->getName(), []);
+  }
+  
+  public function close(){
+    $this->getResource()->save();
+  }
+  
+  public function getResource(){
+    return $this->resource;
+  }
 }
